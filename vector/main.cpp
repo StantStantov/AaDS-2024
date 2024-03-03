e65c2b517276af1b76cd9d4820ab24b58fe810b8
StantStantov <ibragimovimil0@gmail.com>
2024-03-03 14:06:05 +0300

    Add vector declaration

--- vector/main.cpp
+++ vector/main.cpp
@@ -1,4 +1,24 @@
 #include <iostream>
+#include <memory>
+
+namespace ibr
+{
+  template <class T> 
+  class vector
+  {
+  private:
+    std::unique_ptr<T[]> array_ = nullptr;
+    size_t size_ = 0;
+    size_t length_ = 0;
+  public:
+    vector();
+    vector(const vector&);
+    vector(vector&&);
+    vector& operator=(const vector&);
+    vector& operator=(vector&&);
+    ~vector();
+  };
+} // namespace ibr
 
 int main()
 {
